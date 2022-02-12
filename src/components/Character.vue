ed1a6bb59c41b926e5ae8b618443be0f28ffa59b
Mohamed Gallah <mohamed.gallah@gmail.Com>
2022-02-12 21:27:54 +0100

    :lipstick: Replace inline svgs with HeroIcons components

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -11,19 +11,10 @@
           {{ store.currentCharacter.status }}
         </Pill>
         <span class="flex justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 stroke-current text-gray-300" fill="none"
-               viewBox="0 0 24 24"
-               stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg>
+          <LocationMarkerIcon class="h-6 w-6 mr-2 stroke-current text-gray-300" />
           Location: {{ store.currentCharacter.location?.name }}</span>
         <span class="flex justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 stroke-current text-gray-300" fill="none"
-               viewBox="0 0 24 24"
-               stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-          </svg>
+          <HomeIcon class="h-6 w-6 mr-2 stroke-current text-gray-300" />
           Origin: {{ store.currentCharacter.origin?.name }}</span>
       </section>
     </div>
@@ -34,6 +25,7 @@
 import {onUnmounted} from "vue";
 import { useRoute } from 'vue-router';
 import { useStore } from '@/store';
+import { LocationMarkerIcon, HomeIcon } from '@heroicons/vue/outline';
 
 const route = useRoute();
 const store = useStore();
