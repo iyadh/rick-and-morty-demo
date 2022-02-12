00610653e0cff738e9e1c74bcadacb5bff29b122
Mohamed Gallah <mohamed.gallah@gmail.Com>
2022-02-12 22:23:27 +0100

    :truck: Move routes & router to its own file

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
 import { createApp } from 'vue';
 import { createPinia } from "pinia";
-import { createRouter, createWebHashHistory } from 'vue-router';
 import './assets/css/styles.css';
 import App from './App.vue';
-import Characters from "./components/Characters.vue";
-import Character from "./components/Character.vue";
+import router from "./router";
 
-const routes = [
-  { path: '/characters', name: 'home', component: Characters },
-  { path: '/character/:id', name: 'character', component: Character },
-  { path: '/', redirect: '/characters' },
-];
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
 
 createApp(App)
   .use(createPinia())
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+import Characters from "@/components/Characters.vue";
+import Character from "@/components/Character.vue";
+import NoResults from "@/components/shared/NoResults.vue";
+
+const routes = [
+  { path: '/characters', name: 'home', component: Characters },
+  { path: '/character/:id', name: 'character', component: Character },
+  { path: '/', redirect: '/characters' },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NoResults },
+];
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
