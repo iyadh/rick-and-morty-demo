70f7e5fcc90a83af2f93494d9cdea187c6679761
Mohamed Gallah <mohamed.gallah@gmail.com>
2025-08-18 01:53:32 +0100

    🔥 Project cleanup

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import './assets/css/styles.css';
-import App from './App.vue';
-import router from './router';
-
-createApp(App).use(createPinia()).use(router).mount('#app');
